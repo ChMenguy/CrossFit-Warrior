1de168ca634acca98a48c108c37a8025fc84ceed
Christophe MENGUY <christophemenguy0@gmail.com>
2021-03-16 20:43:16 +0100

    mise en place du formulaire d'ajout de performance et bouton retour

---
+++ src/components/cardinput.jsx
@@ -0,0 +1,19 @@
+function Cardinput({title,type,addperftime,handleperftime,addperfrep,handleperfrep,event}){
+    return(
+        <div className="card cardwod" style={{"width" : "18em"}}>
+            <div className="card-body description">
+                <div className="header">
+                    <div type = 'button' className="backbutton" onClick={event}>🔙</div> 
+                    <h2 className="card-title">{title}</h2>
+                    <div></div>
+                </div> 
+                <input class="form-control" type="text" placeholder="Pseudo"></input>
+                {type === 'time' ? <input class="form-control" type="time" value={addperftime} onChange={handleperftime} id="example-time-input"></input> 
+                : <input class="form-control" type="number" value={addperfrep} onChange={handleperfrep} id="example-number-input"></input>}
+                <button type="button" class="btn btn-warning ajout">Envoyer</button>
+            </div>
+        </div>
+    )
+}
+
+export default Cardinput
--- src/components/displaywods.jsx
+++ src/components/displaywods.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import Cardwod from '../components/cardwod'
+import Cardinput from '../components/cardinput'
 import useFetchWods from '../components/fetchWods'
 
 
 function Displaywods ({url}) {
     const [addScore,handleAddScore] = useState({status : false,
                                                 wod :''});
+    const [addPerftime,handlePerftime] = useState('00:00')
+    const [addPerfrep,handlePerfrep] = useState('00')
     const [wodsgirl] = useFetchWods(url)
 
     return(
@@ -14,15 +17,22 @@ function Displaywods ({url}) {
         <div className="container wods">
        
             {wodsgirl.wods.map(wod=>(
-                (addScore.status && addScore.wod==wod.title)?  
-                  wod.title  
+                (addScore.status && addScore.wod===wod.title)?  
+                  <Cardinput 
+                    title = {wod.title}
+                    type = {wod.type}
+                    event = {()=>handleAddScore({status : false, wod : ''})}
+                    addperftime = {addPerftime}
+                    addperfrep = {addPerfrep}
+                    handleperftime = {(e)=>handlePerftime()}           
+                  />  
                 : <Cardwod 
-                        state = {addScore}
-                        title = {wod.title} 
-                        exercices={wod.exercices.map(exercices=><p>{exercices}</p>)} 
-                        tours={wod.rounds==='1' ? 'Tour Unique' : wod.rounds+' tours'}
-                        key = {wod.title}
-                        event = {()=>handleAddScore({status : true, wod : wod.title})}    
+                    state = {addScore}
+                    title = {wod.title} 
+                    exercices={wod.exercices.map(exercices=><p>{exercices}</p>)} 
+                    tours={wod.rounds}
+                    key = {wod.title}
+                    event = {()=>handleAddScore({status : true, wod : wod.title})}    
                     />    
             ))
          } 
